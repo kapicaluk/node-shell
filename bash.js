9d1ec54abb72d54225fedb170735eb7fd94e23b2
kapicaluk <kapicaluk@gmail.com>
2017-09-18 10:53:34 -0400

    pwd and date commands working

--- bash.js
+++ bash.js
@@ -2,10 +2,21 @@
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
+process.stdin.on('data', function(data) {
+   var cmd = data.toString().trim(); // remove the newline
+   if (cmd === 'pwd') {
+   		process.stdout.write("Your current working directory is " + process.cwd());
+    } else if(cmd === 'date') {
+    	let date = new Date().toUTCString();
+    	process.stdout.write("Today's date is " + date);
+    }
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+    else {
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write('\nprompt > ');
+    }
 
 });
+
+
+// process.cwd()
